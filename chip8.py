b1e08000860be34f21cdc8e512a3fc55e4a5546d
RemmyLee <detroitguy@gmail.com>
2024-01-15 01:15:44 -0500

    Color change.

--- chip8.py
+++ chip8.py
@@ -23,7 +23,7 @@ class Chip8:
         )
 
         # Set up OpenGL context
-        glClearColor(0.0, 0.0, 0.0, 1.0)  # Black background
+        glClearColor(0.44, 0.53, 0.0, 1.0)  # Black background
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
         glOrtho(0, 64, 32, 0, -1, 1)  # Coordinate system setup
@@ -601,7 +601,7 @@ class Chip8:
         """Draws the current display state to the screen using OpenGL."""
         glClear(GL_COLOR_BUFFER_BIT)
 
-        glColor3f(1.0, 1.0, 1.0)  # White color for 'on' pixels
+        glColor3f(0.25, 0.32, 0.11)  # White color for 'on' pixels
 
         glBegin(GL_QUADS)
         for x in range(64):  # The display is 64x32 pixels
