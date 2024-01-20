9ddcf9a93a0295ff3ab9f074305e48952c9c0ca0
RemmyLee <detroitguy@gmail.com>
2024-01-20 16:39:44 -0500

    Sound rewrite.

--- system/sound.py
+++ system/sound.py
@@ -1,13 +1,30 @@
 import pygame
-import os
+import array
 
 
 class Chip8Sound:
     def __init__(self):
         pygame.mixer.init()
-        current_script_path = os.path.dirname(__file__)
-        beep_sound_path = os.path.join(current_script_path, "..", "BEEP", "beep.ogg")
-        self.beep_sound = pygame.mixer.Sound(beep_sound_path)
+        self.beep_frequency = 400
+        self.beep_duration = 0.2
+        self.sample_rate = 44100
+        self.beep_sound = self.generate_beep_sound()
+
+    def generate_beep_sound(self):
+        num_samples = int(self.sample_rate * self.beep_duration)
+        wave = array.array("h")
+
+        for t in range(num_samples):
+            value = (
+                32767
+                if (t * self.beep_frequency // self.sample_rate) % 2 == 0
+                else -32767
+            )
+            wave.append(int(0.1 * value))
+
+        sound = pygame.mixer.Sound(buffer=wave.tobytes())
+        sound.set_volume(0.1)
+        return sound
 
     def play_sound(self, sound_timer):
         if sound_timer > 0:
